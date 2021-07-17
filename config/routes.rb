fa9fb76cf0778e5ad9370415a5e2c53c3ea76e15
Elton Santos <eltonaxl@hotmail.com>
2021-07-17 03:27:59 -0300

    Removed useless files and configuring root route to orders#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :orders
+  root to: 'orders#index'
+
+  resources :orders, only: %i[index, show, create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
